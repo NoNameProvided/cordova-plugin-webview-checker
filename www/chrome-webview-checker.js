687416bad2863bad403d170a0bb8e5af71ea2d30
Attila Olah <NoNameProvided@users.noreply.github.com>
2017-04-07 11:17:12 +0200

    fix: refactor js to es5

--- www/chrome-webview-checker.js
+++ www/chrome-webview-checker.js
@@ -1,10 +1,13 @@
-const promisifyCordovaExec = (command, params = [], className = 'ChromeWebviewChecker') => {
-  return new Promise((resolve, reject) => {
+function promisifyCordovaExec(command, params, className) {
+  params = params || [];
+  className = className || 'ChromeWebviewChecker';
+
+  return new Promise(function innerPromise(resolve, reject) {
     cordova.exec(resolve, reject, className, command, params);
   });
 }
 
 module.exports = {
-  isChromeWebviewInstalled: () => { return promisifyCordovaExec('isChromeWebviewInstalled', ['com.google.android.webview']) },
-  getChromeWebviewVersion: () => { return promisifyCordovaExec('getChromeWebviewVersion', ['com.google.android.webview']) }
+  isChromeWebviewInstalled: function isChromeWebviewInstalled() { return promisifyCordovaExec('isChromeWebviewInstalled', ['com.google.android.webview']) },
+  getChromeWebviewVersion: function getChromeWebviewVersion() { return promisifyCordovaExec('getChromeWebviewVersion', ['com.google.android.webview']) }
 }
